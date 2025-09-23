97656a7bedba6e38e43e9ae75940c7cb6624e03b
bitwalt <walterm21@proton.me>
2025-09-23 19:06:20 +0100

    chore: update dependencies and modify icon configurations
    - Updated Tauri dependencies in package.json to newer versions for improved functionality and security.
    - Adjusted icon configurations in tauri.conf.json, adding new icon files and removing the outdated logo.icns.
    - Added new 64x64 and icon.icns files to the icons directory for better resolution support.

--- src/components/Layout/Modal/Withdraw/components/WithdrawForm.tsx
+++ src/components/Layout/Modal/Withdraw/components/WithdrawForm.tsx
@@ -654,7 +654,7 @@ const WithdrawForm: React.FC<WithdrawFormProps> = ({
           {/* Amount Input - Only needed when not specified in invoice */}
           {(addressType === 'bitcoin' ||
             addressType === 'lightning-address' ||
-            (addressType === 'rgb' && !decodedRgbInvoice?.amount)) && (
+            (addressType === 'rgb' && !decodedRgbInvoice?.assignment)) && (
             <div className="space-y-1">
               <Controller
                 control={control}
